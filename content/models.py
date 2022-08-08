c9912e1145fb3c52c191f46b8c4f8aaaef09ff26
TimRas <martinusras95@gmail.com>
2022-08-08 22:16:15 +0000

    data models structure

--- content/models.py
+++ content/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=70, unique=True)
+    slug = models.SlugField(max_length=70, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
+    content = models.TextField()
+    image = models.URLField(max_length=200)
+    excerpt = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+
+
+    class Meta:
+        ordering = ['-created_date']
+
+
+    def __str__(self):
+        return str(self.title)
+
+
+def amount_of_likes(self):
+    return self.likes.count()
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
+    body = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+
+
+    class Meta:
+        ordering = ['created_date']
+
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.author}'
+
+
+# will add amount of posts and amount of comments
+
+class Topics(models.Model):
+    title = models.CharField(max_length=70, unique=True)
+    image = models.URLField(max_length=200)
+    
+    
