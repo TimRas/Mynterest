a3eefb9d42202aa402a00fd391ba73571b7db7a2
TimRas <martinusras95@gmail.com>
2022-08-10 21:16:34 +0000

    create base html

--- content/views.py
+++ content/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Topic
 
-# Create your views here.
+
+class TopicList(generic.ListView):
+    model = Topic
+    template_name = 'index.html'
+    paginate_by = 4
+
