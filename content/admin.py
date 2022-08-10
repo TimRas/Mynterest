fdc37ef31834c2c8b0715168086cab1b657fe936
TimRas <martinusras95@gmail.com>
2022-08-10 20:11:32 +0000

    update data models and customize admin site

--- content/admin.py
+++ content/admin.py
@@ -3,7 +3,28 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment, Topic
 
 
-@admin.register(Post, Comment, Topic)
-class PostandCommentAdmin(SummernoteModelAdmin):
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
 
-    summernote_fields = ('content', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+    list_filter = ('created_date',)
+    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'created_date')
+
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('body')
+    list_filter = ('created_date',)
+    search_fields = ['title', 'body']
+    list_display = ('title', 'body', 'created_date')
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title']
+    list_display = ('title',)
--- content/models.py
+++ content/models.py
@@ -7,10 +7,11 @@ class Post(models.Model):
     slug = models.SlugField(max_length=70, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
     content = models.TextField()
-    image = models.URLField(max_length=200)
+    image = models.URLField(max_length=200, blank=True)
     excerpt = models.TextField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    dislikes = models.ManyToManyField(User, related_name='post_dislikes', blank=True)
 
 
     class Meta:
@@ -27,23 +28,38 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    title = models.CharField(max_length=70, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
     body = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
-
+    dislikes = models.ManyToManyField(User, related_name='comment_dislikes', blank=True)
 
     class Meta:
         ordering = ['created_date']
 
 
     def __str__(self):
+        return str(self.title)
+
+
+    def __str__(self):
         return f'Comment {self.body} by {self.author}'
 
 
-# will add amount of posts and amount of comments and admin use only
+
 class Topic(models.Model):
     title = models.CharField(max_length=70, unique=True)
-    image = models.URLField(max_length=200)
-    
-    
+    slug = models.SlugField(max_length=70, unique=True)
+    image = models.URLField(max_length=200, blank=True)
+    amount_of_posts = models.ManyToManyField(User, related_name='all_posts', blank=True)
+    amount_of_comments = models.ManyToManyField(User, related_name='all_comments', blank=True)
+
+    def __str__(self):
+        return str(self.title)
+
+    def amount_of_posts_in_total(self):
+        return self.amount_of_posts.count()
+
+    def amount_of_comments_in_total(self):
+        return self.amount_of_comments.count()
