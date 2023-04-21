60b01f583b21f892273d174c86d3c2c60e0b5af0
TimRas <martinusras95@gmail.com>
2023-04-21 13:56:43 +0000

    Adjust fields of topic, post and comment models to improve functionality

--- content/models.py
+++ content/models.py
@@ -4,32 +4,27 @@ from django.contrib.auth.models import User
 
 
 class Topic(models.Model):
-    title = models.CharField(max_length=70, unique=True)
+    title = models.CharField(max_length=70, unique=True, null=False, blank=False)
     slug = models.SlugField(max_length=70, unique=True)
     image = models.URLField(max_length=200, blank=True)
-    amount_of_posts = models.ManyToManyField(User, related_name='all_posts', blank=True)
-    amount_of_comments = models.ManyToManyField(User, related_name='all_comments', blank=True)
 
     def __str__(self):
         return str(self.title)
 
 
 class Post(models.Model):
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='topics', default='1')
-    title = models.CharField(max_length=70, unique=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=70, unique=True, null=False, blank=False)
     slug = models.SlugField(max_length=70, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
-    content = models.TextField()
+    content = models.TextField(null=False, blank=False)
     image = models.URLField(max_length=200, blank=True)
-    excerpt = models.TextField(blank=True)
+    excerpt = models.TextField(null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
-    dislikes = models.ManyToManyField(User, related_name='post_dislikes', blank=True)
-
 
     class Meta:
         ordering = ['-created_date']
-
 
     def __str__(self):
         return str(self.title)
@@ -39,7 +34,6 @@ class Post(models.Model):
             self.slug = slugify(self.title)
         super(Post, self).save(*args, **kwargs)
 
-
     def amount_of_likes(self):
         return self.likes.count()
 
@@ -48,17 +42,14 @@ class Comment(models.Model):
     
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
-    body = models.TextField()
+    body = models.TextField(max_length=250, null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
-    dislikes = models.ManyToManyField(User, related_name='comment_dislikes', blank=True)
 
     class Meta:
         ordering = ['created_date']
 
-
     def __str__(self):
-        return str(self.title)
+        return f'Comment {self.body} by {self.author}'
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -66,16 +57,5 @@ class Comment(models.Model):
         super(Comment, self).save(*args, **kwargs)
 
 
-    def __str__(self):
-        return f'Comment {self.body} by {self.author}'
 
 
-
-
-    # def amount_of_posts_in_total(self):
-    #     return self.amount_of_posts.count()
-
-    # def amount_of_comments_in_total(self):
-    #     return self.amount_of_comments.count()
-
-    
