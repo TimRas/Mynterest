85e87c4273c6719c329842b3110e33ef0d44cf2e
TimRas <martinusras95@gmail.com>
2023-05-29 03:44:13 +0000

    Remove static from mynterest urls.py

--- mynterest/settings.py
+++ mynterest/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-
     'cloudinary',
     'crispy_forms',
     'crispy_bootstrap5',
@@ -163,11 +162,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- mynterest/urls.py
+++ mynterest/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import handler404
 
 
@@ -10,6 +8,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')), 
     path('', include("content.urls"), name="home"),
     path('summernote/', include('django_summernote.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 handler404 = 'mynterest.views.handler404'
-
