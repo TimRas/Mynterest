ba75b35258e75c5187242c326e78e44be4fc9fed
TimRas <martinusras95@gmail.com>
2023-05-29 23:57:34 +0000

    Remove static files from cloudinary for Heroku deployment attempt

--- mynterest/settings.py
+++ mynterest/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ["mynterest.herokuapp.com", "localhost"]
 
 
