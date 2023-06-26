bbb6e7e47634a1180531ec2a90ea37f8eeadd0dc
TimRas <martinusras95@gmail.com>
2023-06-26 18:43:16 +0000

    Add new cloudinary urls to image attributes and set DEBUG to False.

--- mynterest/settings.py
+++ mynterest/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from pathlib import Path
 import os
+from pathlib import Path
 from django.contrib.messages import constants as messages
 import dj_database_url
-if os.path.isfile("env.py"):
+if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -107,16 +107,16 @@ WSGI_APPLICATION = 'mynterest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
--- mynterest/urls.py
+++ mynterest/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include("content.urls"), name="home"),
+    path('', include('content.urls'), name="home"),
     path('summernote/', include('django_summernote.urls')),
 ]
